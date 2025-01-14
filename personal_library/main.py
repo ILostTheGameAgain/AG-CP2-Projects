9bc9d02beaaaa8b9f825b9ed864b9babde026c3a
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-01-14 13:10:37 -0700

    started

---
+++ personal_library/main.py
@@ -0,0 +1,28 @@
+#alec george personal library
+
+#will allow you to add and remove things from a list, search for items, and exit the program
+
+#variables, needs a list for books and a set for authors
+books = []
+authors = []
+
+#function to add items
+
+
+
+#function to remove items
+
+
+
+#function to search items
+
+
+
+#main function to run everything
+def main():
+    pass
+
+
+#run the main function until program ends and stupid proof
+while True:
+    main()
