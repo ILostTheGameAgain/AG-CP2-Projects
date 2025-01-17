d722bad403232f2f6f1c6d4e4b4cffe74c501f31
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-01-17 13:43:13 -0700

    finished using a set

--- personal_library/main.py
+++ personal_library/main.py
@@ -4,63 +4,47 @@
 
 
 #function to view the list
-def view(book_list, author_list):
+def view(book_list):
     printed_list = "\nBooks:\n"
-    for i in range(len(book_list)):
-        printed_list += f" {book_list[i]} by {author_list[i]}\n"
+    for i in book_list:
+        printed_list += f" {i}\n"
     return printed_list
 
 
+
 #function to add books
-def add(book_list, author_list, new_book, new_author):
-    book_list.append(new_book)
-    author_list.append(new_author)
-    return (book_list, author_list)
+def add(book_list, new_book, new_author):
+    book_list.add(f"{new_book} by {new_author}")
+    return book_list
     
     
 
 #function to remove books
-def remove(book_list, author_list, removed_item_type, removed_item):
-    if removed_item_type == "1": #remove the item with matching title if input is 1
-        for i in range(len(book_list)):
-            if book_list[i] == removed_item:
-                book_list.pop(i)
-                author_list.pop(i)
+def remove(book_list, removed_item):
+    for i in book_list:
+        if removed_item.strip().lower() in i.strip().lower():
+            book_list.remove(i)
+            return book_list
+        
+    print(f"\n there are no books with {removed_item} in the title or author")
 
-                return book_list, author_list
-            
-        print(f" there isn't a book with the title, {removed_item}")
-    
-    elif removed_item_type == "2": #remove the item with the matching author if input is 2
-        for i in range(len(author_list)):
-            if author_list[i] == removed_item:
-                book_list.pop(i)
-                author_list.pop(i)
-
-                return book_list, author_list
-            
-        print(f" there isn't a book with the author, {removed_item}")
-    
-    else: #stupid proof
-        print("\n invalid input")
-        return book_list, author_list
 
 
 #function to search for books or authors
-def search(book_list, author_list, searched_item):
-    searched_list = f"books with {searched_item} in title or author:"
-    for i in range(len(book_list)):
-        if searched_item in book_list[i] or searched_item in author_list[i]:
-            searched_list += f"\n {book_list[i]} by {author_list[i]}"
+def search(book_list, searched_item):
+    searched_list = f"\nbooks with {searched_item} in title or author:"
+    for i in book_list:
+        if searched_item in i.strip().lower():
+            searched_list += f"\n {i}"
     return searched_list
+
 
 
 
 #main function
 def main():
-    #list for books and authors
-    books = ["The Way of Kings", "Words of Radiance", "Oathbringer", "Rhythm of War", "Wind and Truth", "What If", "Math Textbook"]
-    authors = ["Brandon Sanderson", "Brandon Sanderson", "Brandon Sanderson", "Brandon Sanderson", "Brandon Sanderson", "Randal Monroe", "whoever wrote the math textbook"]
+    #list for books and authors with some stuff already in it
+    books = {"The Way of Kings by Brandon Sanderson", "Words of Radiance by Brandon Sanderson", "Oathbringer by Brandon Sanderson", "Rhythm of War by Brandon Sanderson", "Wind and Truth by Brandon Sanderson", "What If by Randal Monroe", "Math Textbook by someone... i don't know who"}
     #while loop to continuously run other functions
     while True:
         #take user input for what to do, and stupid proof
@@ -70,25 +54,30 @@ def main():
  2 to add a book
  3 to remove a book
  4 to search for a book
+ 5 to end the program
 your answer here: """))
         except ValueError:
             print("\ninvalid input")
             continue
         
         if action == 1: #if action is 1, view the list
-            print(view(books,authors))
+            print(view(books))
         
         elif action == 2: #if action is 2, add a book and its author
-            books, authors = add(books, authors, input("\nwhat book would you like to add? "), input("what is the book's author? "))
+            books = add(books, input("\nwhat book would you like to add? ").strip().lower().capitalize(), input("what is the book's author? ").strip().lower().capitalize())
 
         elif action == 3: #if action is 3, remove a book and its author
-            books, authors = remove(books, authors, input("\n to remove an item with a matching title, type 1\n to remove an item with a matching author, type 2\n your answer here: "), input(" what are you removing? "))
+            books = remove(books, input(" what are you removing? "))
 
         elif action == 4: #if action is 4, search for books and authors
-            print(search(books, authors, input("\nwhat are you searching for? ")))
+            print(search(books, input("\nwhat are you searching for? ").strip().lower()))
+        
+        elif action == 5:
+            break
 
         else: #anything else is an invalid input
             print("\ninvalid input")
 
 #run main function
 main()
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nI lost the game\n\n")
---
+++ test_folder/main.py
@@ -0,0 +1,4 @@
+letters = {1,3,4,2,5}
+for i in range(5):
+    print(letters)
+    letters.pop(i)
