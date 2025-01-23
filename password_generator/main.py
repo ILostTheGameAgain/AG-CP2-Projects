5556c21a4b95090c5ac124f28044200519236e17
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-01-23 13:35:49 -0700

    compacted the main function by making assembling the requirements and assembling the passwords their own function

--- password_generator/main.py
+++ password_generator/main.py
@@ -33,6 +33,43 @@ def special_character(password):
     return password
 
 
+#function to put user requirements into a list to be easily randomized and used
+def assemble_requirements(uppercase, lowercase, number, special, other):
+    requirements = []
+    for i in range(uppercase): #add uppercase letters to the requirements
+        requirements.append(0)
+    for i in range(lowercase): #add lowercase letters to the requirements
+        requirements.append(1)
+    for i in range(number): #add numbers to requirements
+        requirements.append(2)
+    for i in range(special): #add special characters to requirements
+        requirements.append(3)
+    for i in range(other-len(requirements)): #make everything else random
+        requirements.append(random.randint(0,3))
+    
+    return requirements
+
+
+#function to assemble the requirements into a password
+def assemble_password(requirements):
+    #use requirements to make the password
+    full_password = ""
+    for i in range(len(requirements)):
+        random_number = random.randint(0,len(requirements)-1)
+        if requirements[random_number] == 0: #if the value at the random index is 0, add a capital letter
+            full_password = uppercase_letter(full_password)
+        elif requirements[random_number] == 1: #if the value at the random index is 1, add a lowercase letter
+            full_password = lowercase_letter(full_password)
+        elif requirements[random_number] == 2: #if the value at the random index is 2, add a number
+            full_password = number(full_password)
+        elif requirements[random_number] == 3: #if the value at the random index is 3, add a special character
+            full_password = special_character(full_password)
+
+        requirements.pop(random_number) #remove the used value
+
+    return full_password
+
+
 #main function to make the password
 def main():
     print("\nfill in the data below to generate a few random passwords:\n")
@@ -69,39 +106,13 @@ def main():
 
     #repeat a specific number of times
     for x in range(passwords):
-    #assemble the requirements
-        requirements = []
-        for i in range(uppercase_letters): #add uppercase letters to the requirements
-            requirements.append(0)
-        for i in range(lowercase_letters): #add lowercase letters to the requirements
-            requirements.append(1)
-        for i in range(numbers): #add numbers to requirements
-            requirements.append(2)
-        for i in range(special_characters): #add special characters to requirements
-            requirements.append(3)
-        for i in range(length-len(requirements)): #make everything else random
-            requirements.append(random.randint(0,3))
-
-        #use requirements to make the password
-        full_password = ""
-        for i in range(len(requirements)):
-            random_number = random.randint(0,len(requirements)-1)
-            if requirements[random_number] == 0: #if the value at the random index is 0, add a capital letter
-                full_password = uppercase_letter(full_password)
-            elif requirements[random_number] == 1: #if the value at the random index is 1, add a lowercase letter
-                full_password = lowercase_letter(full_password)
-            elif requirements[random_number] == 2: #if the value at the random index is 2, add a number
-                full_password = number(full_password)
-            elif requirements[random_number] == 3: #if the value at the random index is 3, add a special character
-                full_password = special_character(full_password)
-
-            requirements.pop(random_number) #remove the used value
 
         #print message on the first iteration
         if x == 0:
             print("\npossible passwords:")
-        
-        print(f"{full_password}\n") #print the finished password
+
+        #assemble the requirements, use them to assemble the password, and print the password
+        print(f"{assemble_password(assemble_requirements(uppercase_letters, lowercase_letters, numbers, special_characters, length))}\n") #print the finished password
     
     
     
