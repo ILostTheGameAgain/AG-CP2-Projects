bebf9365332cceebc574297cc0e32604d45b4489
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-01-23 13:25:59 -0700

    finished

--- password_generator/main.py
+++ password_generator/main.py
@@ -58,10 +58,52 @@ def main():
             if length < uppercase_letters+lowercase_letters+numbers+special_characters: #more stupid proof
                 print("\ninvalid input\n")
                 continue
+            passwords = int(input("number of passwords: "))
+            if length < 1: #more stupid proof
+                print("\ninvalid input\n")
+                continue
             break
         except ValueError:
             print("\ninvalid input\n")
-    print("e")
 
+
+    #repeat a specific number of times
+    for x in range(passwords):
+    #assemble the requirements
+        requirements = []
+        for i in range(uppercase_letters): #add uppercase letters to the requirements
+            requirements.append(0)
+        for i in range(lowercase_letters): #add lowercase letters to the requirements
+            requirements.append(1)
+        for i in range(numbers): #add numbers to requirements
+            requirements.append(2)
+        for i in range(special_characters): #add special characters to requirements
+            requirements.append(3)
+        for i in range(length-len(requirements)): #make everything else random
+            requirements.append(random.randint(0,3))
+
+        #use requirements to make the password
+        full_password = ""
+        for i in range(len(requirements)):
+            random_number = random.randint(0,len(requirements)-1)
+            if requirements[random_number] == 0: #if the value at the random index is 0, add a capital letter
+                full_password = uppercase_letter(full_password)
+            elif requirements[random_number] == 1: #if the value at the random index is 1, add a lowercase letter
+                full_password = lowercase_letter(full_password)
+            elif requirements[random_number] == 2: #if the value at the random index is 2, add a number
+                full_password = number(full_password)
+            elif requirements[random_number] == 3: #if the value at the random index is 3, add a special character
+                full_password = special_character(full_password)
+
+            requirements.pop(random_number) #remove the used value
+
+        #print message on the first iteration
+        if x == 0:
+            print("\npossible passwords:")
+        
+        print(f"{full_password}\n") #print the finished password
+    
+    
+    
 
 main()
