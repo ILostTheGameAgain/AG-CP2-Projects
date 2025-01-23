a2055b187a9abaea597814e68439c571844f24d7
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-01-23 13:37:05 -0700

    made a message a little more specific

--- password_generator/main.py
+++ password_generator/main.py
@@ -91,7 +91,7 @@ def main():
             if special_characters < 0: #more stupid proof
                 print("\ninvalid input\n")
                 continue
-            length = int(input(f"length of password (must be at least {uppercase_letters+lowercase_letters+numbers+special_characters}):"))
+            length = int(input(f"length of password (must be at least {uppercase_letters+lowercase_letters+numbers+special_characters}, any larger will add random characters):"))
             if length < uppercase_letters+lowercase_letters+numbers+special_characters: #more stupid proof
                 print("\ninvalid input\n")
                 continue
