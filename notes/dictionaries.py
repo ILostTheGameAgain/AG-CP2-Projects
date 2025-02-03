4f420926abe5b1b4c53ff806372f556df161ba82
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-02-03 13:27:56 -0700

    did dictionary notes

---
+++ notes/dictionaries.py
@@ -0,0 +1,32 @@
+#Alec George Dictionaries Notes
+
+#notes on dictionaries
+car = {
+    "make": "Ford",
+    "model": "Escape xlt",
+    "year": 2008,
+    "color": "Red",
+    "name": "Freya"
+}
+
+#print(car["make"])
+
+students = {
+    "first": {
+        1: "Vincent",
+        2: "Cecily",
+        3: "Evan",
+        4: "Sawyer",
+    },
+    "sixth": {
+        1: "Nicole",
+        2: "Luke", 
+        3: "Gavin",
+        4: "Jackson"
+    }
+}
+
+
+#print(students["sixth"][2])
+
+print(car.values())
