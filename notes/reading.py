2305bcbedfba63b5d35e17c25cfb8fa84378fadd
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-02-10 13:25:27 -0700

    finished reading file notes

---
+++ notes/reading.py
@@ -0,0 +1,22 @@
+# Alec George, Reading files python notes
+import csv
+
+#with open("notes/test.txt", "r") as file:
+#    content = file.read()
+#    print(content)
+
+
+#print(content)
+
+
+users  = {}
+
+with open("notes/user_info.csv") as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader)
+    for row in csv_reader:
+        users.update({row[0]: row[1]})
+        print(row)
+
+
+    
