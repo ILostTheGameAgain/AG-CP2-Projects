66ce4d433203560738010dc7e0b4f01da03866c9
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-03-06 13:43:27 -0700

    worked on profiles

--- battle_simulator/profile_management.py
+++ battle_simulator/profile_management.py
@@ -7,11 +7,21 @@ import csv
 from small_functions import *
 
 #function to create a profile, takes a name and gives them basic stats
-def add_profile(name, attack, defense):
+def add_profile(name, attack, defense, health):
     with open("battle_simulator/character_info.csv","a") as file:
-        fieldnames = ["name", "attack", "defense"]
-        csv_writer = csv.writer(file)
-        csv_writer.write(name, attack, defense)
+        file.write(f"{name}, {attack}, {defense}, {health}\n")
 
 
-add_profile("e", 100, 10)
+
+
+
+#function to add new profile
+def new_profile():
+    name = input("\nwhat is the name? ")
+    attack = is_int(input("\nwhat is the attack? "))
+    defense = is_int(input("\nwhat is the defense? "))
+    health = is_int(input("\nwhat is the health? "))
+    add_profile(name,attack,defense,health)
+
+
+new_profile()
