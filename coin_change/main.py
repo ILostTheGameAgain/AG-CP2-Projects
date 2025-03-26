a5c4cb50fc4a546ac30e82060596bc8ff03a28af
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-03-26 13:44:08 -0600

    almost finished

--- coin_change/main.py
+++ coin_change/main.py
@@ -4,33 +4,83 @@
 import pandas as pd
 
 
-#function to see if something is in a list
-def is_in_list(value, items):
+#functoin to see if something is in a list
+def is_in_list(value, items1, items2):
     value = value.strip().lower()
-    if value in items:
-        pass
+    if value in items1:
+        return value
+    
+    elif value in items2:
+        return value
     else:
-        print(f"\nthat is not an optoin. Your options are:\ndollars\npounds\neuros\n")
-        value = is_in_list(input('what cion type are you using?\n'))
+        print(f"\nthat is not an optoin. Your optoins are:\ndollars (us)\npounds (uk)\neuros(no specific place)\nkronor (sweden)\n")
+        value = is_in_list(input('what cion type are you using?\n'), items1,items2)
 
+    return value
+
+
+#functoin to check if something is an integer
+def is_float(value):
+    try:
+        int(value)
+    except:
+        value = is_float(input('\nthat is not a number. Try again\n'))
+
+    return int(value)
+
+
+#functoin to check if something is greater than 0
+def is_positive(value):
+    if value <= 0:
+        value = is_positive(is_float(input('\nthat is not a valid number. Try again\n')))
+        
     return value
 
 
 #main functoin to run everything else
 def main():
     #variable for the available cions
-    cions = []
+    cions = {
+        'name': [],
+        'value': []
+    }
 
     #make cions into a dataframe
     df = pd.read_csv('coin_change/denominations.csv')
+    #make it a dictionary
+    df = df.to_dict()
 
-
-    #function to add the right coin types to the list
+    #functoin to add the right cion types to the list
     def add_coins(ct):
-        for index, typ in enumerate(df['type']):
+        for index, typ in enumerate(df['type'].values()):
             if typ == ct:
-                cions.append{}
+                cions['name'].append(df['name'][index])
+                cions['value'].append(df['value'][index])
 
+        for index, typ in enumerate(df['country'].values()):
+            if typ == ct:
+                cions['name'].append(df['name'][index])
+                cions['value'].append(df['value'][index])
+
+
+    #functoin to calculate the right coins to add to get target amount
+    def caclate(amt):
+        cion_string = 'needed cions/bills/notes:'
+        #do for every coin
+        for i in range(len(cions['name'])):
+            repeats = 0
+            #subtract the coin that can still fit in the amont repeatedly
+            while amt > 0:
+                amt -= cions['value'][-(i+1)]
+                repeats += 1
+
+            if amt < 0:
+                amt += cions['value'][-(i+1)]
+                repeats -= 1
+
+            cion_string += f'\n{repeats} '
+
+            print(amt)
 
     #tell user instructoins
     print("""
@@ -41,5 +91,15 @@ This will then tell you the minimum number of cions, bills, and notes to have th
 """)
     
     #ask user for cion type
-    cion_type = is_in_list(input("\nwhat cion type are you using?\n"))
+    cion_type = is_in_list(input("\nwhat is the name of the cion type/country?\n"), df['type'].values(), df['country'].values())
+    add_coins(cion_type)
 
+    #ask user for amount
+    amount = is_positive(is_float(input(f"\nhow many {cion_type} are you finding?\n")))
+
+    #print the number of cions
+    caclate(amount)
+
+
+
+main()
